111c4b9ae95d37f61dd5f5578f866b38b05281c9
thasup <thanachonfirst@hotmail.com>
2021-12-15 22:26:14 +0700

    Add app.route() for specific article

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.listen(port, () => {
 const password = process.env.MONGODB_PASS;
 main().catch(err => console.log(err));
 async function main() {
-    await mongoose.connect(`mongodb+srv://admin-first:${password}@cluster0.hi5zx.mongodb.net/wikiDB`);
+    await mongoose.connect(`mongodb://localhost:27017/wikiDB`);
 };
 
 // Create collection scheme and module
@@ -39,9 +39,9 @@ const Article = mongoose.model("Article", articleSchema);
 app.route("/articles")
     // GET route
     .get((req, res) => {
-        Article.find({}, (err, foundArticle) => {
+        Article.find({}, (err, foundArticles) => {
             if (!err) {
-                res.send(foundArticle);
+                res.send(foundArticles);
             } else {
                 res.send(err);
             }
@@ -69,9 +69,71 @@ app.route("/articles")
     .delete((req, res) => {
         Article.deleteMany({}, (err) => {
             if (!err) {
-                res.send("Successfullt delete all articles!");
+                res.send("Successfully delete all articles!");
+            } else {
+                res.send(err);
+            }
+        });
+    });
+
+// Request Targetting a Specific Article
+
+app.route("/articles/:articleTitle")
+    // GET route
+    .get((req, res) => {
+        Article.findOne({title: req.params.articleTitle}, (err, foundArticle) => {
+            if (!err) {
+                res.send(foundArticle);
+            } else {
+                res.send("No article matching that title was found.");
+            }
+        });
+    })
+    // PUT route
+    .put((req, res) => {
+        Article.replaceOne(
+            // condition
+            {title: req.params.articleTitle},
+            // update
+            {title: req.body.title, content: req.body.content},
+            // callback function
+            (err) => {
+            if (!err) {
+                res.send("Successfully updated article!");
             } else {
                 res.send(err);
             }
         });
     })
+    // PATCH route
+    .patch((req, res) => {
+        Article.updateOne(
+            // condition
+            {title: req.params.articleTitle},
+            // update
+            req.body,
+            // callback function
+            (err) => {
+            if (!err) {
+                res.send("Successfully updated article!");
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    // DELETE route
+    .delete((req, res) => {
+        Article.deleteOne(
+            // {$or: [
+            //     {_id: req.params.articleTitle}, 
+            //     {title: `${req.params.articleTitle}`}
+            // ]}, 
+            {title: req.params.articleTitle},
+            (err) => {
+            if (!err) {
+                res.send("Successfully deleted article!");
+            } else {
+                res.send(err);
+            }
+        });
+    });
